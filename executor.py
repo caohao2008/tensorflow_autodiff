8f8d61997470e57a879d4bdbb466094787f50e53
caohao <caohao@caohaodeMacBook-Pro.local>
2018-08-28 09:48:52 +0800

    add forward implemention

--- executor.py
+++ executor.py
@@ -7,23 +7,48 @@ class Executor:
 		print "init executor"
 	
 	def buildComputation(self,expr_table):
-		backward_table = {}
+		backward_op_table = {}
 		forward_table = {}
+		forward_input_table = {}
+		forward_op_table = {}
 		for expr in expr_table:
 			print expr
-			forward_table[str(expr.output)]=expr.operator().forward
+			forward_op_table[str(expr.output)]=expr.operator().forward
+			forward_input_table[str(expr.output)]=expr.inputs
 			for input in expr.inputs:
-				backward_table[str(expr.output)+","+input]=expr.operator().backward
-		print forward_table
-		print backward_table
-		return forward_table,backward_table
+				backward_op_table[str(expr.output)+","+input]=expr.operator().backward
+		print forward_op_table
+		print forward_input_table
+		print backward_op_table
+		return forward_op_table,forward_input_table,backward_op_table
 	
-	def execute(self,forward_table,*_input_values):
+	def execute(self,forward_op_table,forward_input_table,_input_values):
 		input_values = _input_values
 		value_table  = {}
-		for key in forward_table:
-			value_table[key]=forward_table[key](_input_values)
-		print value_table
+		##fill in input value
+		#find_values(value_t
+		times = 0
+		while 1:
+			fill_all = 1
+			for key in forward_op_table:
+				cur_input = forward_input_table[key]	
+				cur_values = []
+				skip_value = 0
+				for input in cur_input:
+					print "execute for key "+key
+					if(_input_values.has_key(input)):
+						cur_values.append(_input_values[input])
+					elif(value_table.has_key(input)):
+						cur_values.append(value_table[input])
+					else:
+						skip_value = 1
+				if skip_value!=1:		
+					value_table[key]=forward_op_table[key](cur_values)
+				else:
+					fill_all = 0
+			print "value_table = ",value_table
+			if(fill_all>0):
+				break
+		
+		print "value_table = ",value_table
 		return value_table
-	
-							
--- op.py
+++ op.py
@@ -1,58 +1,71 @@
 import sys
 import os
+import math
 
 class Op:
-	def forward(self,*_inputs):
+	def forward(self,_inputs):
 		print "forward"
+		return "basic forward op"
 	
-	def backward(self,*_inputs):
+	def backward(self,_inputs):
 		print "backward"
+		return "basic backward op"
+		
 
 class Assign:
-	def forward(self,*_inputs):
-		print "forward assign"
+	def forward(self,_inputs):
+		print "forward assign",_inputs
+		return _inputs[0]
 	
-	def backward(self,*_inputs):
+	def backward(self,_inputs):
 		print "backward assign"
-
-
+		return "backward assign"
 
 class Plus(Op):
-	def forward(self,*_inputs):
+	def forward(self,_inputs):
                 print "forward plus"
+		return _inputs[0]+_inputs[1]
 	
-	def backward(self,*_inputs):
+	def backward(self,_inputs):
 		print "backward plus"
  
 class Minus(Op):
-	def forward(self,*_inputs):
+	def forward(self,_inputs):
                 print "forward minus"
+		return _inputs[0]-_inputs[1]
+                return "forward minus",_inputs
 	
-	def backward(self,*_inputs):
+	def backward(self,_inputs):
 		print "backward minus"
  
 class Mult(Op):
-	def forward(self,*_inputs):
+	def forward(self,_inputs):
                 print "forward mult"
+		return _inputs[0]*_inputs[1]
+		return "forward mult",_inputs
 	
-	def backward(self,*_inputs):
+	def backward(self,_inputs):
 		print "backward mult"
 
 
  
 class Sin(Op):
-	def forward(self,*_inputs):
+	def forward(self,_inputs):
                 print "forward sin"
+		return math.sin(_inputs[0])
+		return "forward sin",_inputs
 	
-	def backward(self,*_inputs):
+	def backward(self,_inputs):
 		print "backward sin"
 
  
 class Ln(Op):
-	def forward(self,*_inputs):
+	def forward(self,_inputs):
                 print "forward ln"
+		return math.log(_inputs[0])
+		#return "forward ln",_inputs
 	
-	def backward(self,*_inputs):
+	def backward(self,_inputs):
 		print "backward ln"
 
  
